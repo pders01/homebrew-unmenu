e83be3e0154264f49a977dd4b5a27191ab4a7276
Paul Derscheid <paul.derscheid@lmscloud.de>
2024-03-11 14:17:24 +0100

    chore: initial commit of unmenu cask

---
+++ Casks/unmenu.rb
@@ -0,0 +1,10 @@
+cask "unmenu" do
+  version "0.1"
+  sha256 "94b5dfa84f226f7ab7602a60281380bfec6e6ac7adaa1ea1d1405c0735d2b428"
+
+  url "https://github.com/unmanbearpig/unmenu/releases/download/v0.1/unmenu.app.zip"
+  name "unmenu" 
+  homepage "https://github.com/unmanbearpig/unmenu"
+
+  app "unmenu.app"
+end
